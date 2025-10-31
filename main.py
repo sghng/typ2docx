e714b4e9180740fa75c5e7321e6a4933916795ac
Samuel Huang <hi@sgh.ng>
2025-10-31 12:58:17 -0400

    integrate applescript with py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
-
 import json
 import subprocess
 from pathlib import Path
+from time import sleep
 
 import typer
 from extract import extract as extract_equations  # ty: ignore[unresolved-import]
@@ -31,8 +31,10 @@ def convert(
     path: str = typer.Argument(..., help="Entry point to the Typst project"),
 ):
     """Convert a Typst project to DOCX format."""
+    path: Path = Path(path)
     console.print(f"[bold blue]Converting[/bold blue] {path}...")
-    typ2pdf(Path(path))
+    typ2pdf(path)
+    pdf2docx(path)
 
 
 def typ2pdf(path: Path):
@@ -48,7 +50,7 @@ def typ2pdf(path: Path):
 
     try:
         path.write_text(preamble + src)  # insert preamble in place
-        result = subprocess.run(["typst", "compile", path, "tmp.pdf"])
+        result = subprocess.run(["typst", "compile", path, "typ2docx.a.pdf"])
         if result.returncode != 0:
             raise RuntimeError("Typst compilation failed")
 
@@ -62,6 +64,14 @@ def typ2pdf(path: Path):
             cleaned = src[:start] + src[end:]
             cleaned = cleaned.lstrip()
             path.write_text(cleaned)
+
+
+def pdf2docx(path: Path):
+    # TODO: other engines
+    result = subprocess.run(["./acrobat.applescript"])
+    if result.returncode != 0:
+        raise RuntimeError("Failed to convert pdf -> docx with Acrobat")
+    # sleep(1)  # for safety
 
 
 def main():
