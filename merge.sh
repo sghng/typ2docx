0be5c3f593ccdbbd32b95cf22bb8d5d2c0dc79ae
Samuel Huang <hi@sgh.ng>
2025-10-31 14:27:02 -0400

    a proof of concept xslt implementation

--- merge.sh
+++ merge.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 unzip -q typ2docx.a.docx -d typ2docx.a.d
 unzip -q typ2docx.b.docx -d typ2docx.b.d
 
-echo Pretending to do some operation...
-wc -c typ2docx.a.d/word/document.xml
+saxon -s:typ2docx.a.d/word/document.xml \
+      -xsl:merge.xslt \
+      -o:typ2docx.a.d/word/document.xml \
+      doc-b-path=typ2docx.b.d/word/document.xml
 
 cd typ2docx.a.d # must be zipped from inside!
 zip -r -q ../typ2docx.out.docx *
