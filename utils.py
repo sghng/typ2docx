4de0c69f3205b7527554d13d00846275243eed7a
Samuel Huang <hi@sgh.ng>
2025-11-15 17:15:46 -0500

    wrapper for create_subprocess_exec with error handling

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+from asyncio import CancelledError, create_subprocess_exec
 from contextlib import contextmanager
+from functools import wraps
 from pathlib import Path
+from subprocess import CalledProcessError
 
 
 @contextmanager
@@ -7,3 +10,15 @@ def TempFile(path: Path, content: str = ""):
     path.write_text(content)
     yield path
     path.unlink(missing_ok=True)
+
+
+@wraps(create_subprocess_exec)
+async def run(*args, **kwargs):
+    process = await create_subprocess_exec(*args, **kwargs)
+    try:
+        returncode = await process.wait()
+    except CancelledError:
+        process.terminate()
+        raise
+    if returncode:
+        raise CalledProcessError(returncode, args)
