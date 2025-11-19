7ae3968ccfd6cceb0ee47f65e7a7b111e94b7955
Samuel Huang <hi@sgh.ng>
2025-11-19 00:27:48 -0500

    remove a comment

--- merge.sh
+++ merge.sh
@@ -5,7 +5,6 @@ unzip -o -q a.docx -d a.d
 unzip -o -q b.docx -d b.d
 rsync -a --delete a.d/ out.d/
 
-# python executable set at call site
 python3 -m saxon >out.d/word/document.xml
 
 cd out.d # must be zipped from inside!
