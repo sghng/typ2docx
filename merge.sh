20cce08659d5bece89fbcdb87f300aa485ca9e9f
Samuel Huang <hi@sgh.ng>
2025-11-18 22:27:54 -0500

    update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 unzip -o -q a.docx -d a.d
 unzip -o -q b.docx -d b.d
 rsync -a --delete a.d/ out.d/
 
-saxon -xsl:merge.xslt -it:main -o:out.d/word/document.xml
+# python executable set at call site
+python -m saxon >out.d/word/document.xml
 
 cd out.d # must be zipped from inside!
 zip -r -q ../out.docx *
