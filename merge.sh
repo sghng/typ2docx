f868386b2964fbeaa832e4dc08f5ed58d93a4371
Samuel Huang <hi@sgh.ng>
2025-11-19 21:42:30 -0500

    minor tweak over merge.sh

--- merge.sh
+++ merge.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
-
-unzip -o -q a.docx -d a.d
-unzip -o -q b.docx -d b.d
+rm -rf a.d b.d out.d
+unzip -o -q a.docx -d a.d &
+unzip -o -q b.docx -d b.d &
+wait
 rsync -a --delete a.d/ out.d/
-
 python3 -m saxon >out.d/word/document.xml
-
 cd out.d # must be zipped from inside!
 zip -r -q ../out.docx *
