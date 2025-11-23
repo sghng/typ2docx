9af69bdb86833348c8dda4858f8986c8faecca70
Samuel Huang <hi@sgh.ng>
2025-11-22 20:51:48 -0500

    fix file path problem

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env bun
-import { mkdtemp, rm } from "fs/promises";
-import AdmZip from "adm-zip";
+import { rm, mkdtemp, writeFile } from "fs/promises";
 
 const port = 10000;
 Bun.serve({
@@ -8,43 +7,35 @@ Bun.serve({
 	async fetch(req) {
 		switch (req.method) {
 			case "POST": {
-				const { project, entry } = await req.json();
+				const { project, entry = "main.typ" } = await req.json();
 				if (!project)
 					return new Response("Missing 'project' field", {
 						status: 400,
 					});
-				const zip = new AdmZip(Buffer.from(project, "base64"));
-				const dir = await mkdtemp("");
-				try {
-					await Promise.all(
-						zip
-							.getEntries()
-							.filter((e) => !e.isDirectory)
-							.map((e) =>
-								Bun.write(
-									`${dir}/${e.entryName.split("/").pop()}`,
-									e.getData(),
-								),
-							),
-					);
-					const { stdout, stderr, exitCode } =
-						await Bun.$`cd ${dir} && typ2docx ${entry || "main.typ"} -e pdfservices`
-							.env(process.env)
-							.quiet()
-							.nothrow();
-					return exitCode
-						? new Response(stderr, { status: 500 })
-						: new Response(stdout, {
-								headers: {
-									"Content-Type":
-										"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-									"Content-Disposition":
-										"attachment; filename=output.docx",
-								},
-							});
-				} finally {
-					rm(dir, { recursive: true });
-				}
+				const dir = await mkdtemp("/tmp/typ2docx-");
+				await writeFile(
+					`${dir}/project.zip`,
+					Buffer.from(project, "base64"),
+				);
+				await Bun.$`cd ${dir} && unzip -o project.zip && rm project.zip`
+					.quiet()
+					.nothrow();
+				const { stdout, stderr, exitCode } =
+					await Bun.$`cd ${dir} && typ2docx ${entry} -e pdfservices -- --root .`
+						.env(process.env)
+						.quiet()
+						.nothrow();
+				await rm(dir, { recursive: true, force: true });
+				return exitCode
+					? new Response(stderr, { status: 500 })
+					: new Response(stdout, {
+							headers: {
+								"Content-Type":
+									"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+								"Content-Disposition":
+									"attachment; filename=output.docx",
+							},
+						});
 			}
 			case "GET":
 				return new Response(Bun.file("index.html"));
