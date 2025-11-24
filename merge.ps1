460b6be4b5b83b8f75af76309d06ac5eed2aa576
Samuel Huang <hi@sgh.ng>
2025-11-24 12:50:17 -0500

    minor tweaks

--- merge.ps1
+++ merge.ps1
@@ -2,15 +2,13 @@
 $ErrorActionPreference = "Stop"
 'a', 'b' | ForEach-Object {
     Remove-Item "$_.d" -Recurse -ErrorAction SilentlyContinue
-    Copy-Item "$_.docx" "$_.zip" -Force
+    Copy-Item "$_.docx" "$_.zip"
     Expand-Archive "$_.zip" "$_.d"
 }
-robocopy "a.d" "out.d" /mir | Out-Null
-python -m saxon | Out-File "out.d/word/document.xml"
+robocopy a.d out.d /mir > $null
 if ($LASTEXITCODE -ge 8) { throw "robocopy failed with exit code $LASTEXITCODE" }
-try {
-    Push-Location "out.d"
-    Compress-Archive "*" "../out.docx" -Force
-} finally {
-    Pop-Location
-}
+python -m saxon > "out.d/word/document.xml"
+if ($LASTEXITCODE -ne 0) { throw "python/saxon failed with exit code $LASTEXITCODE" }
+Push-Location out.d
+try { Compress-Archive * ../out.zip -Force } finally { Pop-Location }
+Move-Item out.zip out.docx -Force
