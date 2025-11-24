5165a2bb7270a26f7d62749e7fc6f2a1c8cb008e
Samuel Huang <hi@sgh.ng>
2025-11-24 12:26:34 -0500

    better path handling

--- merge.ps1
+++ merge.ps1
@@ -8,5 +8,9 @@ $ErrorActionPreference = "Stop"
 robocopy "a.d" "out.d" /mir | Out-Null
 python -m saxon | Out-File "out.d/word/document.xml"
 if ($LASTEXITCODE -ge 8) { throw "robocopy failed with exit code $LASTEXITCODE" }
-Push-Location "out.d"
-Compress-Archive -Force "*" "../out.docx"
+try {
+    Push-Location "out.d"
+    Compress-Archive "*" "../out.docx" -Force
+} finally {
+    Pop-Location
+}
