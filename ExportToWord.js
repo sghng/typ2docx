9d1fa93762fd011893c21814f32fde83e9bc2521
Samuel Huang <hi@sgh.ng>
2025-11-26 00:37:08 -0500

    updated ExportToWord.js with a saveable path

--- ExportToWord.js
+++ ExportToWord.js
@@ -2,22 +2,13 @@
 // Place in: ~/Library/Application Support/Adobe/Acrobat/DC/JavaScripts/
 
 ExportToWord = app.trustedFunction(function () {
+  var outputPath =
+    "/Macintosh HD/Users/sghuang/Library/Containers/com.adobe.Acrobat.Pro/Data/tmp/test.docx";
   app.beginPriv();
-
-  var doc = this;
-  var docPath = doc.path;
-  var pathSep = docPath.indexOf("\\") >= 0 ? "\\" : "/";
-  var lastSep = docPath.lastIndexOf(pathSep);
-  var docDir = docPath.substring(0, lastSep);
-  var docName = docPath.substring(lastSep + 1);
-  var baseName = docName.replace(/\.pdf$/i, "");
-  var outputPath = docDir + pathSep + baseName + ".docx";
-
   doc.saveAs({
     cPath: outputPath,
     cConvID: "com.adobe.acrobat.docx",
     bPromptToOverwrite: false,
   });
-
   app.endPriv();
 });
